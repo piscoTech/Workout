79e30add96e53a10736fbc01e1f64fcb9edb6c1d
Marco Boschi <boschi95@gmail.com>
2016-01-15 10:14:53 +0100

    Initial Commit

---
+++ Workout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Workout
+//
+//  Created by Marco Boschi on 15/01/16.
+//  Copyright © 2016 Marco Boschi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
